3b4b22fadf4ecd8f2c915ceea9f709f040840739
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-10-16 16:28:18 +0100

    Rspec test Pass

---
+++ lib/docking_station.rb
@@ -0,0 +1,22 @@
+class DockingStation 
+
+  def release_bike
+
+  end 
+end 
+
+
+
+
+
+
+# station1 = DockingStation.new 
+# station2 = DockingStation.new 
+
+
+# docking_station.release_bike
+
+
+
+
+
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,11 @@
+require 'docking_station.rb'
+
+describe DockingStation do 
+    it "release_bike when" do
+        # station = DockingStation.new 
+     expect(DockingStation.new.release_bike).to eq ()
+  end
+
+end 
+
+
