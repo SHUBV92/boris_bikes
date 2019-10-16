58ac772567429d2507219e150718e6434aa386db
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-10-16 18:02:04 +0100

    Chp10 Borris Bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,14 @@
 require 'docking_station'
 
 describe DockingStation do 
-    it "release_bike when" do
-        # station = DockingStation.new 
-     expect(DockingStation.new.release_bike).to eq ()
-    end
-end 
+it { is_expected.to respond_to :release_bike }
 
+                                   
 
+it 'releases working bikes' do 
+    bike = subject.release_bike 
+    expect(bike).to be_working
+  end 
+end
 
+  
